0a218fc584323758afd54ef8d1b05ab8a8190e2f
Carlos Henrique <caique.h@hotmail.com>
2024-11-29 05:56:35 -0300

    fix: enhance Avatar component with additional shadow effects and improved structure

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -5,9 +5,10 @@ export default function Avatar() {
   return (
     <div className={` relative h-[12.5rem] w-[12.5rem] rounded-full`}>
       <div className="h-full w-full overflow-hidden rounded-full">
-        <Image src={AvatarImage} placeholder="blur"	alt="My profile picture." />
+        <Image src={AvatarImage} placeholder="blur" alt="My profile picture." />
       </div>
-      <div className="animate-spin absolute top-0 h-[12.5rem] w-[12.5rem] overflow-hidden rounded-full shadow-[5px_5px_35px_0px_#9f7aea,-5px_-5px_35px_0px_#5a67d8]"></div>
+      <div className="absolute top-0 h-[12.5rem] w-[12.5rem] animate-spin overflow-hidden rounded-full shadow-[5px_5px_35px_0px_#9f7aea,-5px_-5px_35px_0px_#5a67d8]"></div>
+      <div className="absolute top-0 h-[12.5rem] w-[12.5rem] animate-spin overflow-hidden rounded-full shadow-[inset_5px_5px_35px_0px_#9f7aea,-5px_-5px_35px_0px_#5a67d8] "></div>
     </div>
   );
-} 
+}
