fbb84c20dc7a52f99ede859ebf29301e6c84e36c
Carlos Henrique <caique.h@hotmail.com>
2024-12-09 18:18:07 -0300

    feat: update Navbar component to adjust width of an empty div

--- src/components/ui/navbar/nav-bar.tsx
+++ src/components/ui/navbar/nav-bar.tsx
@@ -6,7 +6,7 @@ export default function Navbar() {
     <div className="sticky z-10 w-full">
       <header className="fixed flex w-full flex-col items-center justify-center bg-transparent bg-opacity-10 bg-clip-padding backdrop-blur-md backdrop-filter">
         <div className="flex h-full w-full items-center justify-between px-2 py-4">
-          <div></div>
+          <div className="w-[1.2rem]"></div>
           <div className="animate-fadein select-none">
             {'<'}carlosh-
             <span className={'bg-gradient-to-r from-violet-500 to-purple-400 bg-clip-text text-transparent'}>dev</span>
