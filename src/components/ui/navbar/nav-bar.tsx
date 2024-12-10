69242532b29a0715e4324d9c7cf5fc8806cc0933
Carlos Henrique <caique.h@hotmail.com>
2024-12-10 10:13:48 -0300

    feat: update Navbar layout to use grid for better spacing and adjust width of an element

--- src/components/ui/navbar/nav-bar.tsx
+++ src/components/ui/navbar/nav-bar.tsx
@@ -5,8 +5,8 @@ export default function Navbar() {
   return (
     <div className="sticky z-10 w-full">
       <header className="fixed flex w-full flex-col items-center justify-center bg-transparent bg-opacity-10 bg-clip-padding backdrop-blur-md backdrop-filter">
-        <div className="flex h-full w-full items-center justify-between px-2 py-4">
-          <div className="w-[1.2rem]"></div>
+        <div className="grid h-full w-full grid-cols-[repeat(3,_minmax(1.2rem,_auto))] items-center justify-between gap-2 px-2 py-4">
+          <div className="w-[2.25rem]"></div>
           <div className="animate-fadein select-none">
             {'<'}carlosh-
             <span className={'bg-gradient-to-r from-violet-500 to-purple-400 bg-clip-text text-transparent'}>dev</span>
